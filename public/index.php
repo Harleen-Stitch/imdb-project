d7b17d978a55a07eadbf126a843fac4ccae3c43b
Kenza-trafarski <kenza.trafarski@supinfo.com>
2026-02-04 00:47:30 +0100

    maj index (msg erreur inutile car ds db.php)

--- public/index.php
+++ public/index.php
@@ -10,18 +10,10 @@ include dirname(__DIR__) . '/includes/header.php'; # header html
 ?>
 
 <main>
-    <h1>Hello World!</h1>
+    <h1 style='color: green'>Hello World!</h1>
     <p>Bienvenue sur IMDB Project</p>
     
-    <?php
-    // Test de connexion BDD
-    try {
-        $stmt = $pdo->query("SELECT 1");
-        echo "<p style='color: green;'>✅ Connexion BDD réussie !</p>";
-    } catch (PDOException $e) {
-        echo "<p style='color: red;'>❌ Erreur de connexion BDD</p>";
-    }
-    ?>
+
 </main>
 
 <?php include dirname(__DIR__) . '/includes/footer.php'; ?>
